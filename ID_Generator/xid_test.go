0ed794e58c2638eff0c2727228fd23ba321d0e7a
vector <carmeltop@hotmail.com>
2019-08-23 20:19:41 +0800

    nsq demo

---
+++ ID_Generator/xid_test.go
@@ -0,0 +1,16 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/rs/xid"
+)
+
+func TestID(t *testing.T) {
+	guid := xid.New()
+	println(guid.String())
+	println(guid.Value)
+	println(guid.Pid())
+	println(guid.Time().String())
+	println(guid.Counter())
+}
